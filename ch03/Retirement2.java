efb6579983ba4827c5fadccee1c18bb9d39dadda
Xinghai Ge <g18845381272@163.com>
2025-06-03 17:43:33 +0800

    [feat] do-while loop

--- ch03/Retirement2.java
+++ ch03/Retirement2.java
@@ -2,17 +2,12 @@ package ch03;
 
 import java.util.Scanner;
 
-/**
- * 该程序展示 while 循环
- */
-public class Retirement {
+public class Retirement2 {
     public static void main(String[] args) {
+        
         Scanner in = new Scanner(System.in);
 
-        System.out.print("你需要多少退休金？￥");
-        double goal = in.nextDouble();
-
-        System.out.print("你每年净收入？￥");
+        System.out.print("你每年净收入？");
         double payment = in.nextDouble();
 
         System.out.print("年利率：");
@@ -21,12 +16,18 @@ public class Retirement {
         double balance = 0;
         int years = 0;
 
-        while (balance < goal) {
+        String input;
+        do {
             balance += payment;
             double interest = balance * interestRate / 100;
             balance += interest;
             years++;
-        }
+
+            System.out.printf("%d年后，你的余额是￥%,.2f\n", years,balance);
+
+            System.out.println("准备退休吗（Y/N）");
+            input = in.next();
+        }while (input.equals("N"));
 
         System.out.printf("你可以在%d年后退休。\n", years);
         in.close();
